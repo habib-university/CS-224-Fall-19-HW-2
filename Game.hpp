a9d27d0aa28cc8c768958d9e3cb823a9141370c4
arham97 <54774711+arham97@users.noreply.github.com>
2019-10-09 10:26:12 +0500

    updated game.h

--- Game.hpp
+++ Game.hpp
@@ -1,16 +1,22 @@
+#pragma once
+#include "Player.hpp"
+#include "Map.hpp"
+
 class Game {
 private:
   Map m_map;        // the map to be played.
-  Player m_player;  // the player.
+  Player m_player;// the player.
   int m_days;       // the number of days.
   bool m_game_over; // is the game over?
 
 public:
-  Game(); // initialize attributes, place player on the start of the map.
+  Game();
 
   void run(); // the game loop.
 
   void execute_special(std::string); // execute the given special.
 
   void show_state(); // show current game state.
+
+  void show_intro();
 };
